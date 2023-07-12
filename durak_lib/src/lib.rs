9893a4859b05a90a69908b51bab813496a0bc364
ClayenKitten <dima070704@yandex.ru>
2023-07-12 19:16:27 +0300

    Add features and two card structs to `durak_lib`

--- durak_lib/src/lib.rs
+++ durak_lib/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[cfg(feature = "bevy")]
+use bevy_ecs::component::Component;
+
+#[cfg(feature = "serde")]
+use serde::{Serialize, Deserialize};
+
+use strum::EnumIter;
+
+#[derive(EnumIter, Debug, PartialEq, Eq, Clone, Copy)]
+#[cfg_attr(feature = "bevy", derive(Component))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum CardSuit {
+    Clover,
+    Diamond,
+    Heart,
+    Pike,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(EnumIter, Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "bevy", derive(Component))]
+#[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
+pub enum CardRank {
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Jack,
+    Queen,
+    King,
+    Ace,
 }
