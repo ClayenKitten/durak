83cc574547ded6504cf731fc37b99624ec76c138
ClayenKitten <dima070704@yandex.ru>
2023-07-19 01:00:15 +0300

    Add `AuthenticateHost` extractor

--- durak_server/src/auth.rs
+++ durak_server/src/auth.rs
@@ -13,12 +13,6 @@ use crate::state::AppState;
 /// Extractor that authenticates player.
 pub struct Authenticate(pub AuthentificatedPlayer);
 
-#[derive(Debug)]
-pub struct AuthentificatedPlayer {
-    pub game_id: GameId,
-    pub player_id: PlayerId,
-}
-
 #[async_trait]
 impl FromRequestParts<AppState> for Authenticate {
     type Rejection = (StatusCode, &'static str);
@@ -49,4 +43,56 @@ impl FromRequestParts<AppState> for Authenticate {
 
         Ok(Authenticate(AuthentificatedPlayer { game_id, player_id }))
     }
+}
+
+/// Extractor that authenticates player and ensures that it is host of the game.
+pub struct AuthenticateHost(pub AuthentificatedPlayer);
+
+#[async_trait]
+impl FromRequestParts<AppState> for AuthenticateHost {
+    type Rejection = (StatusCode, &'static str);
+
+    async fn from_request_parts(
+        parts: &mut Parts,
+        state: &AppState,
+    ) -> Result<Self, Self::Rejection> {
+        const REJECTION: (StatusCode, &'static str) =
+            (StatusCode::UNAUTHORIZED, "Host authorization failed");
+
+        let Some(header) = parts.headers.get(AUTHORIZATION) else {
+            tracing::debug!("Failed auth");
+            return Err(REJECTION);
+        };
+        let Ok(header) = header.to_str() else {
+            tracing::debug!("Failed auth");
+            return Err(REJECTION);
+        };
+        let Ok(AuthHeader { game_id, player_id, token }) = serde_json::from_str::<AuthHeader>(&header) else {
+            tracing::debug!("Failed auth");
+            return Err(REJECTION);
+        };
+        if !state.auth.validate(token, game_id, player_id) {
+            tracing::debug!("Failed auth for game `{game_id}`");
+            return Err(REJECTION);
+        }
+        match state
+            .games
+            .with_game(game_id, |game| game.host == player_id)
+        {
+            Some(true) => Ok(AuthenticateHost(AuthentificatedPlayer {
+                game_id,
+                player_id,
+            })),
+            None | Some(false) => {
+                tracing::debug!("Failed auth for game `{game_id}`: not a host");
+                Err(REJECTION)
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AuthentificatedPlayer {
+    pub game_id: GameId,
+    pub player_id: PlayerId,
 }
