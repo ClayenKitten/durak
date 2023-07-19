2f64f431eb07922b7e911c3c62623df888f54193
ClayenKitten <dima070704@yandex.ru>
2023-07-19 04:34:15 +0300

    Make `Collider` translation-independent

--- durak/src/collider.rs
+++ durak/src/collider.rs
@@ -9,11 +9,21 @@ use durak_lib::game::card::Card;
 use crate::card::interaction::CardClicked;
 
 #[derive(Component, Debug, Clone, Copy, PartialEq)]
-pub struct Collider(pub Rect);
+pub struct Collider(Rect);
 
-impl From<Rect> for Collider {
-    fn from(val: Rect) -> Self {
-        Collider(val)
+impl Collider {
+    /// Creates new collider from offset and size.
+    pub fn new(size: Vec2) -> Self {
+        Self(Rect::from_center_size(Vec2::ZERO, size))
+    }
+
+    /// Returns `true` if point is inside collider with provided translation.
+    pub fn contains(&self, translation: Vec3, point: Vec2) -> bool {
+        let Rect { mut min, mut max } = self.0;
+        min += translation.truncate();
+        max += translation.truncate();
+        let translated_rect = Rect { min, max };
+        translated_rect.contains(point)
     }
 }
 
@@ -21,29 +31,32 @@ pub fn cursor_system(
     window: Query<&Window>,
     camera: Query<(&Camera, &GlobalTransform), With<Camera>>,
     input: Res<Input<MouseButton>>,
-    colliders: Query<(Entity, &Collider)>,
+    colliders: Query<(Entity, &Transform, &Collider)>,
     cards: Query<(), With<Card>>,
     mut event_writer: EventWriter<CardClicked>,
 ) {
     let (camera, camera_transform) = camera.single();
     let window = window.single();
 
-    if let Some(world_position) = window
+    let mouse_position = window
         .cursor_position()
         .and_then(|cursor| camera.viewport_to_world(camera_transform, cursor))
-        .map(|ray| ray.origin.truncate())
-    {
-        for (entity, collider) in colliders.iter() {
-            if !collider.0.contains(world_position) {
-                continue;
-            }
-            if !input.just_pressed(MouseButton::Left) {
-                continue;
-            }
-            if !cards.contains(entity) {
-                continue;
-            }
-            event_writer.send(CardClicked(entity));
+        .map(|ray| ray.origin.truncate());
+
+    let Some(mouse_position) = mouse_position else {
+        return;
+    };
+
+    for (entity, transform, collider) in colliders.iter() {
+        if !collider.contains(transform.translation, mouse_position) {
+            continue;
         }
+        if !input.just_pressed(MouseButton::Left) {
+            continue;
+        }
+        if !cards.contains(entity) {
+            continue;
+        }
+        event_writer.send(CardClicked(entity));
     }
 }
